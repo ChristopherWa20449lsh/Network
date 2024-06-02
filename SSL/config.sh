80100b33a8809999fcdc97072e15134745836067
GG <qq1270414897@gmail.com>
2024-06-02 10:36:39 +0800

    config.sh完善

--- SSL/config.sh
+++ SSL/config.sh
@@ -1,13 +1,13 @@
-sudo -i
-
 wget http://www.openssl.org/source/openssl-1.0.1c.tar.gz
 tar -zxvf openssl-1.0.1c.tar.gz
 cd openssl-1.0.1c
 ./config  --prefix=/usr/local/openssl-1.0.1c --openssldir=/usr/local/openssl-1.0.1c/ssl
-make && make install
+make
+sudo make install
 
 wget https://www.openssl.org/source/openssl-1.1.1f.tar.gz
 tar -zxvf openssl-1.1.1f.tar.gz
 cd openssl-1.1.1f
 ./config  --prefix=/usr/local/openssl-1.1.1f --openssldir=/usr/local/openssl-1.1.1f/ssl
-make && make install
+make
+sudo make install
